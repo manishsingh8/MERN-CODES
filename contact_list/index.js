dd044fa95231ab7173178ed414996070ec98ab7c
Manish Singh <manishsingh04031998@gmail.com>
2023-07-03 12:30:16 +0530

    Setting_Template_Engine

--- contact_list/index.js
+++ contact_list/index.js
@@ -1,18 +1,54 @@
-
 const express = require('express');
 const path = require('path');
 
 const port =8100;
-
-
 const app = express();
+
+// contact list
+let contact = [
+    {
+        name:'manish',
+        city:'varanasi'
+    },
+    {
+        name:'prashant',
+        city:'uttarakhand'
+    },
+    {
+        name:'sandeep',
+        city:'agra'
+    }
+
+]
+
+//setting template engine
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname, 'views'));
 
 
+
 app.get('/',function(req,res){
-     return res.render('home');
+     return res.render('home',{
+        title:"Contact List",
+        contact_list:contact
+    }
+     
+     );
 });
+
+
+
+app.get('/practice',function(req,res){
+    return res.render('practice',{title:"Practice Page "});
+});
+
+
+app.post('/contact-list',function(req,res){
+    // return res.redirect('/practice');
+})
+
+
+
 
 
 app.listen(port,function(err){
@@ -20,4 +56,4 @@ app.listen(port,function(err){
         console.log("Error",err);
     }
     console.log("Express is running on port:",port);
-})
+});
