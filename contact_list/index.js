58fe7f8cbf40951d5fc054767278ca752dadcfe8
Manish Singh <manishsingh04031998@gmail.com>
2023-07-07 18:27:00 +0530

    Complete_First_Project

--- contact_list/index.js
+++ contact_list/index.js
@@ -2,11 +2,10 @@ const express = require("express");
 const path = require("path");
 
 const port = 8100;
-const db = require('./config/mongoose');
-const Contact = require('./models/contact');
+const db = require("./config/mongoose");
+const Contact = require("./models/contact");
 
 const app = express();
-
 
 //setting template engine
 app.set("view engine", "ejs");
@@ -16,14 +15,13 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded());
 
 // Reading the Static file assets
-app.use(express.static('assets'));
-
+app.use(express.static("assets"));
 
 // contact list
 let contact = [
   {
     name: "manish",
-    city: "varanasi",
+    id: "varanasi",
   },
   {
     name: "prashant",
@@ -36,14 +34,17 @@ let contact = [
 ];
 
 app.get("/", function (req, res) {
-  return res.render("home", {
-    title: "Contact List",
-    contact_list: contact,
-  });
-});
-
-app.get("/practice", function (req, res) {
-  return res.render("practice", { title: "Practice Page " });
+  Contact.find({})
+    .then((dbContacts) => {
+      return res.render("home", {
+        title: "Contact List",
+        contact_list: dbContacts,
+      });
+    })
+    .catch((err) => {
+      console.log("Error in fetching dbContacts");
+      return;
+    });
 });
 
 // rendering the second demopage
@@ -62,29 +63,34 @@ app.post("/contact-list", function (req, res) {
   // contact.push(req.body);
 
   Contact.create({ name: req.body.name, city: req.body.city })
-  .then(newContact => {
-    console.log('********', newContact);
-    return res.redirect('back');
-  })
-  .catch(err => {
-    console.log('error in creating a contact:', err);
-    // Handle the error appropriately
-    return res.redirect('back');
-  });
+    .then((newContact) => {
+      console.log("********", newContact);
+      return res.redirect("back");
+    })
+    .catch((err) => {
+      console.log("error in creating a contact:", err);
+      // Handle the error appropriately
+      return res.redirect("back");
+    });
 });
 
 // deleting the contact
-app.get('/delete-contact',(req,res)=>{
-   let city = req.query.city;
-   let index = contact.findIndex(contact => contact.city === city);//it will return -1 if no city found otherwise indexnumber
-   if(index != -1){
-    contact.splice(index,1);
-   }
-   return res.redirect('back');
-})
+app.get("/delete-contact", (req, res) => {
+  // get the id from parameters id
+  let id = req.query.id;
+  console.log(id);
 
-
-
+  // fint the contact with id in databse and delete it
+  Contact.findByIdAndDelete(id)
+  .then((id)=>{
+        console.log('contact deleted successfully');
+        return res.redirect('back');
+   })
+   .catch((err)=>{
+    console.log('Error while deleting the contact');
+    return;
+   })
+});
 
 app.listen(port, function (err) {
   if (err) {
